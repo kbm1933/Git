384444d51908c3d7f84485dae2ed628eaeed1c3d
kbm1933 <wjsalsgh456@gmail.com>
2022-09-13 16:27:08 +0900

    파이썬 클래스 활용 과제 커밋

---
+++ Class Task/profile.py
@@ -0,0 +1,44 @@
+from pprint import pprint
+
+
+class Profile():
+    def __init__(self):
+        self.profile = {
+            "name": "-",
+            "gender": "-",
+            "birthday": "-",
+            "age": "-",
+            "phone": "-",
+            "email": "-",
+        }
+    def set_profile(self,profile):
+        self.profile = profile
+    def get_name(self):
+        return self.profile["name"]
+    def get_gender(self):
+        return self.profile["gender"]
+    def get_birthday(self):
+        return self.profile["birthday"]
+    def get_age(self):
+        return self.profile["age"]
+    def get_phone(self):
+        return self.profile["phone"]
+    def get_email(self):
+        return self.profile["email"]
+
+profile = Profile()
+profile.set_profile({
+    "name": "Kim",
+    "gender": "man",
+    "birthday": "06/09",
+    "age": 28,
+    "phone": "01012341234",
+    "email": "python@sparta.com",
+})
+
+print(profile.get_name())
+print(profile.get_gender())
+print(profile.get_birthday())
+print(profile.get_age())
+print(profile.get_phone())
+print(profile.get_email())
